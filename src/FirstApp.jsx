16d374ddf74a16eaf551ea8bc74f9550811acec9
Fredy <fredy.huachaca21@gmail.com>
2023-03-26 10:10:52 -0500

    Variables en HTML

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,9 +1,20 @@
 
 export const FirstApp = () => {
+  const nombre = 'Fredy'
+  const ahora = new Date();
+
+const opciones = {
+  dateStyle: 'short',
+  timeStyle: 'short',
+};
+
+const fechaYHora = ahora.toLocaleString(undefined, opciones);
+console.log(fechaYHora);
   return (
     <>
-    <h1>Fredy</h1>
+    <h1>{nombre}</h1>
     <h2>Huachaca</h2>
+    <h3>fecha y hora: {fechaYHora}</h3>
     </>
   )
 }
