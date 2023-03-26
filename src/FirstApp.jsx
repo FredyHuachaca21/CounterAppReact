9f85a550c88897944103d915c093aadf96e1dd4b
Fredy <fredy.huachaca21@gmail.com>
2023-03-26 10:17:18 -0500

    Impresión de objeto en html

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,4 +1,10 @@
 
+const cliente = {
+  nombre: 'Fredy',
+  apellido: 'Huachaca',
+  telefono: '+51 9643156473'
+}
+
 export const FirstApp = () => {
   const nombre = 'Fredy'
   const ahora = new Date();
@@ -15,6 +21,9 @@ console.log(fechaYHora);
     <h1>{nombre}</h1>
     <h2>Huachaca</h2>
     <h3>fecha y hora: {fechaYHora}</h3>
+
+    <code>{JSON.stringify( cliente )}</code>
+
     </>
   )
 }
