c203a3d89cc3801887503a5768fff5597a16bc5a
Fredy <fredy.huachaca21@gmail.com>
2023-03-26 12:26:13 -0500

    Evento onClick

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({value}) => {
+ 
+  const menos = (event) => {console.log(event)}
+
   return (
     <>
     <h1>CounterApp</h1>
     <h2>{value}</h2>
-    <button type="button" className="p-3 m-2 btn btn-outline-primary">-</button>
+    <button type="button" className="p-3 m-2 btn btn-outline-primary"
+    onClick={menos}>-</button>
     <button type="button" className="p-3 m-2 btn btn-outline-secondary">Reset</button>
     <button type="button" className="p-3 m-2 btn btn-outline-success">+</button>
 
